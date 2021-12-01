0e7215992bc2e9a1f8c567d04eced2f21e5f82b0
Nidhin P P <nidhin.p@bfly.ae>
2021-12-01 15:03:02 +0530

    image rendering fix

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -9,7 +9,7 @@ function Works() {
                         <div className="col-lg-4 col-sm-6">
                             <div className="row justify-content-start align-items-center card-border">
                                 <div className="card" style={{width: "18rem"}}>
-                                    <div className="react-back"/>
+                                    <div className="img-bg" style={{backgroundImage:"url(../img/tools/react.png)"}}/>
                                     <div className="card-body">
                                         <h5 className="card-title">CoWin API Integration</h5>
                                         <p className="card-text">Consumed CoWin API and created a web application that can list availiable vaccine slots.</p>
@@ -21,7 +21,7 @@ function Works() {
                         <div className="col-lg-4 col-sm-6">
                             <div className="row justify-content-start align-items-center card-border">
                                 <div className="card" style={{width: "18rem"}}>
-                                    <div className="js-back"/>
+                                    <div className="img-bg" style={{backgroundImage:"url(../img/tools/js.png)"}}/>
                                     <div className="card-body">
                                         <h5 className="card-title">eCommerce Websites</h5>
                                         <p className="card-text">Designed and developed UI for various attractive eCommerce Websites.</p>
@@ -33,7 +33,7 @@ function Works() {
                         <div className="col-lg-4 col-sm-6">
                             <div className="row justify-content-start align-items-center card-border">
                                 <div className="card" style={{width: "18rem"}}>
-                                    <div className="psql-back"/>
+                                    <div className="img-bg" style={{backgroundImage:"url(../img/tools/psql.png)"}}/>
                                     <div className="card-body">
                                         <h5 className="card-title">Buisness Applications</h5>
                                         <p className="card-text">Worked with e-commerce databases to develop functional components.</p>
